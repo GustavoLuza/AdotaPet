d541c0b629f76a5f45f0501e957d98679f985ac9
Gustavo Rafael Luza <gustavoluza@hotmail.com>
2019-07-24 10:09:39 -0300

    Inicio implementação de verificação da data de validade

---
+++ Src/GL.Treinamento.Domain/Specifications/Mantimento/MantimentoDeveEstarDentroDaValidadeSpecification.cs
@@ -0,0 +1,20 @@
+﻿using DomainValidation.Interfaces.Specification;
+using GL.Treinamento.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GL.Treinamento.Domain.Specifications.Mantimento
+{
+    public class MantimentoDeveEstarDentroDaValidadeSpecification : ISpecification<Mantimentos>
+    {
+        public bool IsSatisfiedBy(Mantimentos mantimentos)
+        {
+            /* DateTime validade = Convert.ToDateTime(mantimentos.DataValidade);
+             return DateTime.Now - validade >= DateTime.Now;*/
+            return true;
+        }
+    }
+}
